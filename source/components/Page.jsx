588af2b87684acc23cd2c2581eb7a56edf55bb56
rgoldfinger <rgoldfinger@coursera.org>
2015-06-10 09:47:03 -0700

    hello world

---
+++ source/components/Page.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+
+
+require('./__styles__/App.styl');
+
+var WriteApp = React.createClass({
+
+  render() {
+    return (
+      <div>
+        Hello Scrolling world
+      </div>
+    );
+  }
+});
+
+module.exports = WriteApp;
